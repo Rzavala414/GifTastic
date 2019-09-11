30e256c972c75e95f4f3753f7065ccd8a9e60e1e
Roger Zavala <Rogeliozavala15@gmail.com>
2019-09-10 23:00:46 -0700

    incorporated giphy api and added rating,gif,sticker,limit tages. also got three buttons displaying on front end

--- assets/Javascript/app.js
+++ assets/Javascript/app.js
@@ -1,6 +1,32 @@
-//TODO:Link Giphy CDN
+// this array stores the values of the GIFs we'll choose from
+var myGIf = ["lions", "tigers", "bears"];
+//Link Giphy CDN
+    var queryURLStatic = `https://api.giphy.com/v1/stickers/search?q=dragon-ball-ball-z&limit=10&rating=pg&api_key=xKtabkdViFrxRn5oMum5q8ysKXiEX62t`;
+    var queryURLAnimated = `https://api.giphy.com/v1/gifs/search?q=batman&limit=10&rating=pg&api_key=xKtabkdViFrxRn5oMum5q8ysKXiEX62t`;
 
-//TODO:create  3 button that has a certain theme on it and have it display to the front end
+$.ajax({
+    url:queryURLStatic,
+    method:"GET"
+
+}).then(function(response){
+
+
+
+
+
+
+});
+
+
+//TODO:create  3 button that has a certain theme on it from the "myGifs" array and have it display to the front end
+for (let i = 0; i < myGIf.length; i++) {
+    var gifButton = $("<button>");
+    gifButton.text(myGIf[i]);
+    $("#gif-buttons").append(gifButton);
+
+}
+
+
 //TODO:Get the buttons to have a click event
 //TODO:when the user clicks the buttons have it display 10 GIFs on to the page that are still images with their rating underneath
 //TODO:when the user clicks on the the image have the GIF play
