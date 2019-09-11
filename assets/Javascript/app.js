506fb915d6fc13385ab1c87244dde45d4fc7c6c4
Roger Zavala <Rogeliozavala15@gmail.com>
2019-09-10 21:39:20 -0700

    added jquery library made basic file structure inside of index.html

---
+++ assets/Javascript/app.js
@@ -0,0 +1,7 @@
+//TODO:Link Giphy CDN
+
+//TODO:create  3 button that has a certain theme on it and have it display to the front end
+//TODO:Get the buttons to have a click event
+//TODO:when the user clicks the buttons have it display 10 GIFs on to the page that are still images with their rating underneath
+//TODO:when the user clicks on the the image have the GIF play
+//TODO:
