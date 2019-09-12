0aaf0abb33e8b4bdcfc22cd8137346a53bb0cfae
Roger Zavala <Rogeliozavala15@gmail.com>
2019-09-11 22:44:42 -0700

    added data of GIF rating to page

--- assets/Javascript/app.js
+++ assets/Javascript/app.js
@@ -1,25 +1,32 @@
 // this array stores the values of the GIFs we'll choose from
 var myGIf = ["batman", "mike-tyson", "dragon-ball-Z"];
-//Link Giphy CDN
+//Linked Giphy CDN
 function getGifs(topic) {
+    //Grabs 10 stickers of a GIF with Pg
     var queryURLStatic = `https://api.giphy.com/v1/stickers/search?q=${topic}&limit=10&rating=pg-13&api_key=xKtabkdViFrxRn5oMum5q8ysKXiEX62t`;
+
     var queryURLAnimated = `https://api.giphy.com/v1/gifs/search?q=${topic}&limit=10&rating=pg-13&api_key=xKtabkdViFrxRn5oMum5q8ysKXiEX62t`;
     $.ajax({
         url: queryURLStatic,
         method: "GET"
 
     }).then(function (response) {
-        console.log(response);
+        
         //loop through all the images
             for (let i = 0; i < response.data.length; i++) {
-                //create a new image html element
+                //gives access to url data
                 var url = response.data[i].images["480w_still"].url
-                //add img url
+                //gives access to rating 
+                var rating = response.data[i].rating
+                //adds rating to page
+                $("#gifs-to-screen").prepend("Rating:" + rating);
+                //adds giphy sticker to screen
                 var gifImage = (`<img src="${url}">`)
                 //adds GIFs on to main screen
                 $("#gifs-to-screen").prepend(gifImage);
-            } 
-        
+
+             
+            }
 
     });
 
@@ -28,23 +35,28 @@ function getGifs(topic) {
 //Creates three buttons with my three themes and displays them to the page
 function renderButtons() {
     for (let i = 0; i < myGIf.length; i++) {
+        //creates a new button tag for item in "myGIF" array
         var gifButton = $("<button>");
+        //adding a class to array buttons
+        gifButton.addClass("gif-btn");
+        //adding a data-attribute
+        gifButton.data("data-name",myGIf[i]);
+        //providing the initial button text
         gifButton.text(myGIf[i]);
-        gifButton.data("name",myGIf[i]);
+        //adding the button to the gif-buttons div
         $("#gif-buttons").append(gifButton);
     }
     
 }
 
 $(document).on("click","button",function(){
-    var name = $(this).data("name")
-    console.log(name);
+    var name = $(this).data("data-name")
     getGifs(name);
 
 });
 
 
-renderButtons()
+renderButtons();
 
 
 //TODO:when the user clicks the buttons have it display 10 GIFs on to the page that are still images with their rating underneath
